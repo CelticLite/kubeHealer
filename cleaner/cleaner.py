9b9a28d39fc111b3e2f5d0fbd1ea98196f5d410c
Obi <celticlite@proton>
2025-06-06 01:40:22 -0400

    cleaned up file structure and removed old unused deps

---
+++ cleaner/cleaner.py
@@ -0,0 +1,56 @@
+import os
+import subprocess
+
+from vllm import LLM, SamplingParams
+
+
+class Cleaner:
+    def __init__(self, config: dict={}, debug=None, executable='echo'):
+        self.llm = None
+        self.params = None
+        self.config = config
+        self.start_vllm(config)
+        self.debug = debug
+        self.executable = executable
+        pass
+
+    def start_vllm(self, config):
+        self.llm = LLM(model=config.get('data',{}).get('model',"meta-llama/Llama-3.2-3B-Instruct"), dtype=config.get('data',{}).get('dtype',"float16"), tensor_parallel_size=config.get('data',{}).get('tensor_parallel_size',4))
+        self.params = SamplingParams(temperature=config.get('data',{}).get('temperature',0.7), top_p=config.get('data',{}).get('top_p',0.9), max_tokens=config.get('data',{}).get('max_tokens',2048))
+
+    def generate_prompts(self, target_dir: str=None):
+        prompts = []
+        if not target_dir:
+            target_dir = input("directory to clean: ").strip()
+        for file in os.listdir(target_dir):
+            if self.debug: print(f"Eating {file}")
+            if os.path.isdir(f"{target_dir}/{file}"):
+                continue # only read files in current directory 
+            file_contents = read_file(f"{target_dir}/{file}")
+            try:
+                with open(path, 'r') as f:
+                    file_contents = f.read()
+            except Exception as e:
+                continue
+            if not file_contents or file_contents == '':
+            	continue
+            if self.debug: print(file_contents)
+            feedback = kube_lint(f"{target_dir}/{file}")
+            if self.debug: print(feedback)
+            if feedback:
+                # print(feedback)
+                system_prompt = "You are an expert Kubernetes configuration optimizer. Your task is to ingest a Kubernetes YAML configuration file along with feedback on issues found in the file. Based on the feedback, you must generate a corrected Kubernetes configuration file in proper YAML format."
+                user_prompt = f"-----Configuration File Below-----\n{file_contents}\n-----Feedback Below-----\n{feedback}\n------------\nRespond with ONLY the corrected YAML Kubernetes configuration file. Do NOT provide any explaination for the changes made."
+                prompts.append({"filename":file,"prompt":f"{system_prompt}\n{user_prompt}"})
+        return prompts
+
+    def kube_lint(self, file=None):
+        if not file:
+            print("ERROR : NO FILE PROVIDED TO KUBE LINTER")
+            return None
+        r = subprocess.run([f"{self.executable} {file}"], text=True, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        ans = r.stdout
+        if "No lint errors found!" in ans:
+            return None
+        if self.debug: print(ans)
+        return ans
---
+++ single_pass.py
@@ -0,0 +1,67 @@
+import argparse
+import json
+
+def process_args():
+    parser = argparse.ArgumentParser(description ='Fix errors found by a linter')
+    parser.add_argument('-t','--target', help ='target directory containing dirty yaml', default=None)
+    parser.add_argument('--debug', help ='run in debug mode', action="store_true")
+    parser.add_argument('-c','--clean', help ='output directory containing cleaned yaml', default=None)
+    return parser.parse_args()
+
+def build_options(file=None):
+    if file:
+        with open(file, 'r') as f:
+            opts = json.load(f)
+    else:
+        opts = {'version' : '1', 'data' : {'model' : 'meta-llama/Llama-3.2-3B-Instruct', 'dtype': 'float16', 'tensor_parallel_size': 4}}
+    return opts
+
+def main():
+    wrong = 0
+    right = 0 
+
+    args = parse_args()
+    if args.options:
+        opts = build_options(file=args.options)
+    else:
+        opts = build_options()
+    c = Cleaner(config=opts, debug=args.debug, executable="kube-linter lint")
+    if args.debug: print(f"SamplingParams set: {c.params}")
+    prompts = c.generate_prompts(target_dir=args.target)
+    try:
+        if args.debug: print(prompts)
+        llm_batched_input = [p.get("prompt",None) for p in prompts]
+        outputs = c.llm.generate(llm_batched_input, sampling_params=c.params)
+        print("All outputs collected")
+    except Exception as e:
+        print(e)
+        outputs = []
+        pass
+    if not args.clean:
+        clean_dir = input("output directory to write new yaml to: ").strip()
+    else:
+        clean_dir = args.clean
+    for i, output in enumerate(outputs):
+        prompt = output.prompt
+        res = ""
+        for o in output.outputs:
+            res += o.text
+        if '```yml' in res:
+            res = res.split('```yml')[1]
+            res = res.split('```')[0]
+        if '```yaml' in res:
+            res = res.split('```yaml')[1]
+            res = res.split('```')[0]
+        with open(f'{clean_dir}/{prompts[i].get("filename","something")}', 'w') as f:
+            f.write(res)
+        still_wrong = c.kube_lint(f'{clean_dir}/{prompts[i].get("filename","something")}')
+        if still_wrong:
+            print(f'File: {prompts[i].get("filename","something")} still has errors')
+            wrong += 1
+        else
+            right += 1
+    print(f"Guessed {right} correct fixes!")
+    print(f"Missed {wrong} corrections.")
+
+if __name__ == '__main__':
+    main()
